f73f6a7b0eda0cac5b98c11ea7630c04b8804d38
hamitmizrak <hamitmizrak@gmail.com>
2022-04-17 15:13:51 +0300

    javascript

---
+++ frontend/_5_javascript/index.js
@@ -0,0 +1,107 @@
+//single-line comment
+/* multi-line comment */
+
+//çıktı
+// window.alert("Pop-up");
+// alert("Pop-up");
+// console.log("Console yani software level");
+// document.write("Html çıktısı");
+
+//variable
+//var deneme;
+// deneme="JavaScript dersi";
+// deneme=3>1;
+//deneme=44;
+//alert(deneme)
+
+//whoisting
+// deneme="JavaScript dersi";
+// var deneme;
+// alert(deneme);
+
+//syntax: ister çift tırnak ister tek tırnak backtick
+// " " ==> çift tırnak
+// ' ' ==> tek tırnak
+// ` ` ==> backtick
+
+// var kelime="Javascript";
+// var tamsayi=44;
+// var virgulluSayi=44.52;
+// var object={};
+// var dizi=[];
+
+//variable syntax(common rules)
+// var kelime=44;
+// var kelime12=44;
+// var $55kelime12=44;
+// var _55kelime12=44;
+// var kelime_diger=44;
+//var sayisalTeknoloji;//camel case
+
+// var 55kelime12=44;
+// var kelime-diger=44;
+// var +55kelime12=44;
+// var ~55kelime12=44;
+
+///////////////////////////////////////////////////////////////////
+//operatörler
+//logic operators      => ||=> veya     &=ve
+//mathematics operator => + - * / %=bölümünden kalan
+//comparison operator  =>  >  >=  <  <=   ==  ===(type)
+//increment x++  ++x
+// var docum=( 6+3*5+3-6/6);
+// document.write(docum)
+
+//typeof: değişken türü nedir
+// var obj=44;
+// var obj=34.55;
+// var obj="merhabalar";
+// var obj={};
+// var obj=[];
+// document.write(typeof obj)
+//////////////////////////////////////////////////////////////////
+//prompt: kullanıcıdan veri almak istediğimizde kullanıyoruz.
+//prompt: dönüş değeri string'tir
+// var sonuc=prompt("Lütfen adınızı giriniz");
+// console.log(sonuc)
+
+// var sayi1,sayi2,toplam=0;
+// sayi1=Number(prompt("Lütfen 1.sayıyı giriniz")) ;
+// sayi2=Number(prompt("Lütfen 2.sayıyı giriniz")) ;
+// toplam=(sayi1+sayi2);
+// console.log(toplam);
+
+//string'i ==> sayıya çevirmek ==>  Number();
+//sayıyı   ==> String çevirmek ==>  String();
+
+//////////////////////////////////////////////////////////////////
+
+//undefined:tanımsız
+ var deneme;
+console.log( deneme)
+
+//NaN: Not a Number
+var result=4/"asd";
+console.log(result)
+
+//Infinity
+var infinite=9/0;
+console.log(infinite);
+
+// new
+// hafızada bir değer oluşturmak
+
+// null: olan hafızayı yok etmek
+var kelime=null;
+console.log(kelime)
+
+// this
+// nesneyi referans etmek
+
+
+
+
+
+
+
+
