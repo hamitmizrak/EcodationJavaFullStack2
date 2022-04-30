38dbfd88a904e38f0cd742cf278fd20b2341e7bc
hamitmizrak <hamitmizrak@gmail.com>
2022-04-30 14:21:47 +0300

    psvm,sout

---
+++ src/main/java/javase/_01_Java.java
@@ -0,0 +1,9 @@
+package javase;
+
+public class _01_Java {
+    //psvm ==> TAB
+    public static void main(String[] args) {
+        //sout ==> TAB
+        System.out.println("Hamit Mızrak 30 Nisan 2022");
+    }
+}
