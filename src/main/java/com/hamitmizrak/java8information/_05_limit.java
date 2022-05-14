1c97614db863af7c6af640cea8a1e18fd44cbc96
hamitmizrak <hamitmizrak@gmail.com>
2022-05-14 15:43:21 +0300

    Sorted Filter Limit

--- src/main/java/com/hamitmizrak/java8information/_05_limit.java
+++ src/main/java/com/hamitmizrak/java8information/_05_limit.java
@@ -8,6 +8,8 @@ import java.util.stream.Collectors;
 
 @Log4j2
 public class _05_limit {
+
+
     public static void main(String[] args) {
 
         List<String> listem55=new ArrayList<String>();
@@ -28,7 +30,7 @@ public class _05_limit {
                 listem55
                         .stream()
                         //.filter( (temp)->"malatya".equals(temp))
-                        .filter( (temp)->"malatya".equals(temp))
+                        .filter( (isa)->"malatya".equals(isa)  )
                         //.filter( (temp)->!"malatya".equals(temp))
                         .limit(2)
                         .collect(Collectors.toList());
