9a5338d1a5a86e93344627977091ba3e24bab1bd
hamitmizrak <hamitmizrak@gmail.com>
2022-05-14 15:22:56 +0300

    forEach Arrays.asList() && List

---
+++ src/main/java/com/hamitmizrak/java8information/_01_ForEachArray.java
@@ -0,0 +1,29 @@
+package com.hamitmizrak.java8information;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.Arrays;
+
+@Log4j2
+public class _01_ForEachArray {
+    public static void main(String[] args) {
+        String [] dizi={"malatya","istanbul","ankara","izmir","bolu","nevsehir"};
+
+        //1.iterative
+        for (int i = 0; i <dizi.length ; i++) {
+            log.info(dizi[i]);
+        }
+        System.out.println("***********************************");
+
+        //2.special For
+        for(String temp : dizi  ){
+            log.info(temp);
+        }
+
+        System.out.println("***********************************");
+        //3. Java 8
+        Arrays.asList(dizi).stream().forEach(System.out::println);
+        System.out.println("***********************************");
+        Arrays.asList(dizi).forEach(System.out::println);
+    }
+}
---
+++ src/main/java/com/hamitmizrak/java8information/_02_ForEachList.java
@@ -0,0 +1,29 @@
+package com.hamitmizrak.java8information;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+public class _02_ForEachList {
+    public static void main(String[] args) {
+
+        List<String> listem=new ArrayList<String>();
+        listem.add("malatya");
+        listem.add("istanbul");
+        listem.add("ankara");
+        listem.add("izmir");
+        listem.add("bolu");
+        listem.add("nevsehir");
+        listem.forEach(System.out::println);
+
+
+
+
+
+
+
+    }
+}
