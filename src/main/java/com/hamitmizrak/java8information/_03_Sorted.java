63185cc3ff0993dc2e5213398976477fd22caa86
hamitmizrak <hamitmizrak@gmail.com>
2022-05-14 15:37:40 +0300

    Sorted Filter Limit

---
+++ src/main/java/com/hamitmizrak/java8information/_03_Sorted.java
@@ -0,0 +1,25 @@
+package com.hamitmizrak.java8information;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log4j2
+public class _03_Sorted {
+    public static void main(String[] args) {
+
+        List<String> listem=new ArrayList<String>();
+        listem.add("malatya");
+        listem.add("istanbul");
+        listem.add("ankara");
+        listem.add("izmir");
+        listem.add("bolu");
+        listem.add("nevsehir");
+
+        List<String> yeniListem=listem.stream().sorted().collect(Collectors.toList());
+        yeniListem.forEach(System.out::println);
+
+    }
+}
