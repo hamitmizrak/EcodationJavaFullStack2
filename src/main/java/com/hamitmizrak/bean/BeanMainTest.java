83fbad106ccddcf511331e5b83bd23e35acdd80f
hamitmizrak <hamitmizrak@gmail.com>
2022-05-21 15:31:12 +0300

    @ResponseBody @GetMapping() @Autowired

--- src/main/java/com/hamitmizrak/bean/BeanMainTest.java
+++ src/main/java/com/hamitmizrak/bean/BeanMainTest.java
@@ -3,6 +3,7 @@ package com.hamitmizrak.bean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BeanMainTest {
@@ -10,8 +11,9 @@ public class BeanMainTest {
     @Autowired
     BeanConfig  beanConfig;
 
-    //http://localhost:8080//bean/beandto
+    //http://localhost:8080/bean/beandto
     @GetMapping("/bean/beandto")
+    @ResponseBody
     public BeanDto getBeanDto(){
         return beanConfig.beanDto();
     }
