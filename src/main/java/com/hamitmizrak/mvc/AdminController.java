95ffeec9b0445e8eff4de12ee79aeada66eda313
hamitmizrak <hamitmizrak@gmail.com>
2022-05-21 17:59:38 +0300

    messages.properties Model Thymeleaf  th:text

--- src/main/java/com/hamitmizrak/mvc/AdminController.java
+++ src/main/java/com/hamitmizrak/mvc/AdminController.java
@@ -1,6 +1,7 @@
 package com.hamitmizrak.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,10 +9,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AdminController {
 
 
-    //http://localhost:8080/controller1
-    @GetMapping("/controller1")
+    //http://localhost:8080/controller0
+    @GetMapping("/controller0")
     @ResponseBody
-    public String getController1(){
+    public String getController0(){
         return "Merhabalar ben @Controllerim";
     }
+
+
+    //http://localhost:8080/controller1
+    @GetMapping("/controller1")
+    public String getController1(){
+        return "thymeleaf1";
+    }
+
+    //http://localhost:8080/controller2
+    @GetMapping("/controller2")
+    public String getController2(Model model){
+        model.addAttribute("key","modelden geldim");
+        return "thymeleaf2";
+    }
+
+
+
 }
