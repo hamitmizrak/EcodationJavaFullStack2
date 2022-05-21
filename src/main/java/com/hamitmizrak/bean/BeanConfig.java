b94c578f0aeab929c069f9155fab02ae6afb1d02
hamitmizrak <hamitmizrak@gmail.com>
2022-05-21 17:45:23 +0300

    bean

--- src/main/java/com/hamitmizrak/bean/BeanConfig.java
+++ src/main/java/com/hamitmizrak/bean/BeanConfig.java
@@ -2,11 +2,13 @@ package com.hamitmizrak.bean;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BeanConfig {
 
-    @Bean
+    @Bean(initMethod = "initialBeanMethod",destroyMethod = "destroyBeanMethod")
+    //@Scope("session")
     public BeanDto beanDto() {
         return BeanDto.builder().beanData("Bean Data 44").beanName("bean adi 44").build();
     }
--- src/main/java/com/hamitmizrak/bean/BeanDto.java
+++ src/main/java/com/hamitmizrak/bean/BeanDto.java
@@ -6,6 +6,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
 
 //lombok
 @Data
@@ -15,13 +19,36 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class BeanDto {
 
+
+
     private long id;
     private String beanName;
     private String beanData;
 
-    public void initialBeanMethod(){
-        log.info("BeanDto Bean Başladı");
-        System.out.println("BeanDto Bean Başladı");
+    public void initialBeanMethod() {
+        log.info("BeanDto Bean Yaşadı");
+        System.out.println("BeanDto Bean Yaşadı");
     }
+
+    public void destroyBeanMethod() {
+        log.info("BeanDto Bean Öldü");
+        System.out.println("BeanDto Bean Öldü");
+    }
+
+
+//    @Autowired
+//    Logger logger;
+//
+//    @PostConstruct
+//    public void init(){
+//        logger.info("Message");
+//    }
+//
+//    public static void main(String[] args) {
+//        BeanDto beanDto = new BeanDto();
+//        System.out.println(beanDto);
+//       //beanDto.init();
+//
+//    }
 
 }
