83eb0300e0758a41acc4b7c2117fc77ce8990fd8
hamitmizrak <hamitmizrak@gmail.com>
2022-05-21 16:22:00 +0300

    @ResponseBody @GetMapping() @Autowired

--- src/main/java/com/hamitmizrak/bean/BeanDto.java
+++ src/main/java/com/hamitmizrak/bean/BeanDto.java
@@ -5,15 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 //lombok
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Log4j2
 public class BeanDto {
 
     private long id;
     private String beanName;
     private String beanData;
+
+    public void initialBeanMethod(){
+        log.info("BeanDto Bean Başladı");
+        System.out.println("BeanDto Bean Başladı");
+    }
+
 }
