3bcea07974307e22d784754e06477b0f49196300
hamitmizrak <hamitmizrak@gmail.com>
2022-05-21 18:12:22 +0300

    thymeleaf3.html

--- src/main/java/com/hamitmizrak/mvc/AdminController.java
+++ src/main/java/com/hamitmizrak/mvc/AdminController.java
@@ -1,5 +1,6 @@
 package com.hamitmizrak.mvc;
 
+import com.hamitmizrak.database.dto.AdminDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,24 +13,34 @@ public class AdminController {
     //http://localhost:8080/controller0
     @GetMapping("/controller0")
     @ResponseBody
-    public String getController0(){
+    public String getController0() {
         return "Merhabalar ben @Controllerim";
     }
 
-
+    //Html
     //http://localhost:8080/controller1
     @GetMapping("/controller1")
-    public String getController1(){
+    public String getController1() {
         return "thymeleaf1";
     }
 
+    //String
     //http://localhost:8080/controller2
     @GetMapping("/controller2")
-    public String getController2(Model model){
-        model.addAttribute("key","modelden geldim");
+    public String getController2(Model model) {
+        model.addAttribute("key", "modelden geldim");
         return "thymeleaf2";
     }
 
+
+    //Object
+    //http://localhost:8080/controller3
+    @GetMapping("/controller3")
+    public String getController3(Model model) {
+        AdminDto adminDto = AdminDto.builder().adminId(5L).name("Adı").surname("Soyadı").build();
+        model.addAttribute("key", adminDto);
+        return "thymeleaf3";
+    }
 
 
 }
