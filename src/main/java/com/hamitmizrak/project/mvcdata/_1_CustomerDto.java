0e398e3d90c9ee2c980e278c8994efcaeb24161b
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 15:39:31 +0300

    CustomerEntity ==>

--- src/main/java/com/hamitmizrak/project/mvcdata/_1_CustomerDto.java
+++ src/main/java/com/hamitmizrak/project/mvcdata/_1_CustomerDto.java
@@ -5,14 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
 //lombok
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
 public class _1_CustomerDto {
+
     private Long customerId;
+
+    @NotEmpty(message = "Adı boş geçilemez")
     private String customerName;
+
+    @NotEmpty(message = "Soyadı boş geçilemez")
     private String customerSurname;
+
+    @NotEmpty(message = "HesCode boş geçilemez")
     private String customerHesCode;
+
+    private Date createdDate;
 }
