dc7dfa9e002093674d5bd310630a439447788269
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-05 18:56:04 +0300

    @RestController - @Entity

--- src/main/java/com/hamitmizrak/project2/_3_TeacherRestController.java
+++ src/main/java/com/hamitmizrak/project2/_3_TeacherRestController.java
@@ -1,13 +1,11 @@
 package com.hamitmizrak.project2;
 
-
 import com.hamitmizrak.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
